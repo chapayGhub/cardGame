4a44d8f209756e909d74cfd176ca5a0436735b98
Stefan Bloch <stefanbloch@gmail.com>
2013-04-24 13:02:12 +0200

    .cs and .csproj files

---
+++ cardgame/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardgame
+{
+    class Card
+    {
+
+        // Variables:
+        private String countryName = "";
+        private int populationSize = 0;
+
+        // Methods:
+        public string Country
+        {
+            get { return countryName; }
+            set { countryName = value; }
+        }
+
+        public int Population
+        {
+            get { return populationSize; }
+            set
+            {
+                if (value > 0) { populationSize = value; }
+                else { Console.WriteLine("Please enter non-negative population size"); }
+            }
+        }
+
+
+        // Constructor:
+        public Card(String cName, int popSize)
+        {
+            countryName = cName;
+            populationSize = popSize;
+        }
+
+        public void printCardInfo()
+        {
+            Console.WriteLine("Population of {0} is {1} people.", this.Country, this.Population);
+        }
+
+    
+    
+    }
+}
---
+++ cardgame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card danmag = new Card("Danmag", 5000000);
+            Card swarje = new Card("Swarje", 9000000);
+            Card murica = new Card("Murica", 300000000);
+            Card inglin = new Card("Inglin", 63000000);
+            //Card canadia = new Card("Canadia", 34000000);
+
+            Deck world = new Deck();
+            world.add(danmag);
+            world.add(swarje);
+            world.add(murica);
+            world.add(inglin);
+            //world.add(canadia);
+            
+            world.print();
+
+            world.shuffle();
+            world.print();
+
+            playerDeck deck1 = new playDeck();
+
+            
+
+
+
+            
+            Console.ReadLine();
+        }
+    } // END PROGRAM
+
+
+
+
+    
+
+    
+} // END NAMESPACE
