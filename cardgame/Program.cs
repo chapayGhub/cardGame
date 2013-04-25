113c5659dbb64fa0ef8692e4293d590da117eddc
Stefan Bloch <stefanbloch@gmail.com>
2013-04-25 12:50:54 +0200

    Main: added comments and clean up

--- cardgame/Program.cs
+++ cardgame/Program.cs
@@ -9,7 +9,8 @@ namespace cardgame
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // Create cards for overall deck:
             Card danmag = new Card("Danmag", 5000000);
             Card swarje = new Card("Swarje", 9000000);
             Card murica = new Card("Murica", 300000000);
@@ -17,7 +18,10 @@ namespace cardgame
             Card canadia = new Card("Canadia", 34000000);
             Card norja = new Card("Norja", 5000000);
 
+            // Create list for overall deck:
             Deck world = new Deck();
+            
+            // Populate overall deck with premade cards:
             world.add(danmag);
             world.add(swarje);
             world.add(murica);
@@ -25,31 +29,31 @@ namespace cardgame
             world.add(canadia);
             world.add(norja);
             
+            // Print the contents of the overall deck:
             //world.print();
 
+            // Shuffle the overall deck:
             //world.shuffle();
             //world.print();
 
+
+            // Create separate player decks:
             playerDeck deck1 = new playerDeck();
             playerDeck deck2 = new playerDeck();
-            playerDeck deck3 = new playerDeck();
-            playerDeck deck4 = new playerDeck();
-            playerDeck deck5 = new playerDeck();
-            playerDeck deck6 = new playerDeck();
-            
+                        
             world.print();
-            world.deal(deck1, deck2, deck3, deck4, deck5, deck6);
+
+            // Deal contents of overall deck into separate player decks:
+            world.deal(deck1, deck2);
             
+            // Print contents of player decks:
             deck1.print();
             deck2.print();
-            deck3.print();
-            deck4.print();
-            deck5.print();
-            deck6.print();
-            world.print();
-            //deck1.draw(deck2);
-            //deck1.print();
-            //deck2.print();
+            
+            
+            // Draw card from player deck:
+            deck1.draw();
+            deck1.print();
 
             
             Console.ReadLine();
