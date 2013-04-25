4607b527f79a20b4d6ccae567085cb2a45e334f7
Grandil <private@nopenope.nope>
2013-04-25 11:09:16 +0200

    Deck: deal(); edited -- playerDeck: draw(); ???
    The deal function can now accomodate a random number of players and deal
    the cards evenly between them. playerDeck mess uploaded for your
    enjoyment!

--- cardgame/Program.cs
+++ cardgame/Program.cs
@@ -14,33 +14,35 @@ namespace cardgame
             Card swarje = new Card("Swarje", 9000000);
             Card murica = new Card("Murica", 300000000);
             Card inglin = new Card("Inglin", 63000000);
-            //Card canadia = new Card("Canadia", 34000000);
+            Card canadia = new Card("Canadia", 34000000);
+            Card norja = new Card("Norja", 5000000);
 
             Deck world = new Deck();
             world.add(danmag);
             world.add(swarje);
             world.add(murica);
             world.add(inglin);
-            //world.add(canadia);
+            world.add(canadia);
+            world.add(norja);
             
-            world.print();
+            //world.print();
 
-            world.shuffle();
-            world.print();
+            //world.shuffle();
+            //world.print();
 
             playerDeck deck1 = new playerDeck();
             playerDeck deck2 = new playerDeck();
+            playerDeck deck3 = new playerDeck();
 
             world.deal(deck1, deck2);
+            world.print();
             deck1.print();
             deck2.print();
-            deck1.draw(deck2);
-            deck1.print();
-            deck2.print();
-            Console.WriteLine(world);
-
-
-
+            deck3.print();
+            world.print();
+            //deck1.draw(deck2);
+            //deck1.print();
+            //deck2.print();
 
             
             Console.ReadLine();
