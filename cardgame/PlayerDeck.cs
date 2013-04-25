d0c23127a2da8329ae0367238e1577d4aeab1840
Stefan Bloch <stefanbloch@gmail.com>
2013-04-25 12:51:30 +0200

    Decks: draw(): Removed arguments

--- cardgame/PlayerDeck.cs
+++ cardgame/PlayerDeck.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace cardgame
 {
-    class playerDeck //: Deck
+    class playerDeck : Deck
     {
         List<Card> cards;
 
@@ -16,67 +16,17 @@ namespace cardgame
         }
 
         //Virker ikke hvis playerDeck er "casted" til Deck???????
-        public void draw(playerDeck player)
+        public void draw()
         {
-            player.add(this.cards.ElementAt(1));
-            this.cards.RemoveAt(1);
+            //player.add(this.cards.ElementAt(1));
+            //this.cards.RemoveAt(1);
             Console.WriteLine(this.cards.Count);
         }
 
 
-        ///////////////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////TEST AF "PLAYERDECK ER ET DECK" FEJL////
-        public void add(Card c)
-        {
-            this.cards.Add(c);
-        }
 
-        public void print()
-        {
-            Console.WriteLine("Deck contains {0} cards:", this.cards.Count);
-            foreach (var card in this.cards)
-                Console.WriteLine("{0}, population: {1}", card.Country, card.Population);
-            Console.WriteLine();
 
-        }
 
-        public void shuffle()
-        {
-            Random rng = new Random();
-            int i, j, k = 0;
-            for (i = 0; i < this.cards.Count * 2; i++)
-            {
-                for (j = 0; j < this.cards.Count; j++)
-                {
-                    k = rng.Next(this.cards.Count);
-                    Card temp = this.cards[j];
-                    this.cards[j] = this.cards[k];
-                    this.cards[k] = temp;
-                }
-            }
 
-        }
-
-        // Deal all cards to players from overall deck and delete it:
-        public void deal(Deck player1, Deck player2)
-        {
-            int fullDeck = this.cards.Count;
-
-            for (int j = 0; j < fullDeck / 2; j++)
-            {
-                player1.add(this.cards.ElementAt(j));
-            }
-            for (int j = fullDeck / 2; j < fullDeck; j++)
-            {
-                player2.add(this.cards.ElementAt(j));
-            }
-
-            this.cards.Clear();
-        }
     }
-
-
-
-
-
 }
