93f95dfd113fb8b0809ddc95eceb6343381e713b
Stefan Bloch <stefanbloch@gmail.com>
2013-04-30 11:16:15 +0200

    Deck abstract class. TotalDeck class

--- cardgame/Deck.cs
+++ cardgame/Deck.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace cardgame
 {
-    class Deck
+    abstract class Deck
     {
         protected List<Card> cards;
 
@@ -49,27 +49,7 @@ namespace cardgame
 
         }
 
-        public void deal(params playerDeck[] players)
-        {
-            
-            int stacks = players.Length;
-            int fullDeck = this.cards.Count;
-            int cardsPerPlayer = fullDeck/stacks;
-
-            this.shuffle();
-            
-            for(int j =  0; j < stacks; j++)
-            {
-                for (int i = cardsPerPlayer-1; i >= 0; i--)
-                {
-                    players[j].add(this.cards.ElementAt(i));
-                    this.cards.RemoveAt(i);
-                }
-            }
-            
-        }
-
-
+        
 
     } // END DECK
 }
