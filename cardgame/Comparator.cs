db811ba17dbbd161a6953e988bbe1179d32a85f5
Stefan Bloch <stefanbloch@gmail.com>
2013-05-07 14:47:11 +0200

    Comparator: Random winner if same values

--- cardgame/Comparator.cs
+++ cardgame/Comparator.cs
@@ -13,6 +13,26 @@ namespace cardgame
             float val1 = p1.compareValue(s1);
             float val2 = p2.compareValue(s1);
 
+            if (val1 == val2)
+            {
+                Random rnd = new Random();
+                // TODO: (20130507, sbm) If more than 2 players this needs to be rewritten:
+                var determineRandomWinner = rnd.Next(1, 3);
+                Console.WriteLine("It's a draw! Player {0} is the randomly chosen winner.", determineRandomWinner);
+                if (determineRandomWinner == 1)
+                {
+                    p1.getLootCard(p1);
+                    p2.getLootCard(p1);
+                }
+                else
+                {
+                    p1.getLootCard(p2);
+                    p2.getLootCard(p2);
+                }
+
+            }
+
+            
             if (val1 > val2)
             {
                 Console.WriteLine("{0} wins the round", p1.getPlayerName );
