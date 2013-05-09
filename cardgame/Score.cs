07c339c57a772dd2004093135a0a8880fc7c3138
Grandil <private@nopenope.nope>
2013-05-09 11:07:52 +0200

    Score
    Nothing important

--- cardgame/Score.cs
+++ cardgame/Score.cs
@@ -8,5 +8,31 @@ namespace cardgame
 {
     class Score
     {
+        //Variables
+
+        //Constructor
+
+        //Methods
+
+        //After each round update the total cardcount of each player
+            //If total cardcount == 0 Player is out
+            //If only one player remains A WINRAR IS HE!
+
+        //Winner screenprint method because awesome!
+
+        public void winrar(string winrarName)
+        {
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("***************    A WINRAR IS YOU!!    ****************");
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("         ***************************************        ");
+            Console.WriteLine("                          " + winrarName);
+            Console.WriteLine("         ***************************************        ");
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("***************    A WINRAR IS YOU!!    ****************");
+            Console.WriteLine("********************************************************");
+        }
+
+
     }
 }
