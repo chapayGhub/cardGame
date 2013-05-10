eb0268b3a328cd852eae305cc8ecd45e30dcc49e
Grandil <private@nopenope.nope>
2013-05-10 12:12:40 +0200

    Conversion from string to card updated

--- cardgame/PopulateDeck.cs
+++ cardgame/PopulateDeck.cs
@@ -26,30 +26,23 @@ namespace cardgame
 
     }
 
-    // TODO: Refactor so string can contain arbitrary number of category-value pairs (if not too difficult).
     static void fixString(string parameters, Deck deck)
     {
+        List<Category> categories = new List<Category>();
         var split = parameters.Split(',');
-        // Adjust to number of arguments that the string of parameters contains.
-        if (split.Length != 5)
-              throw new ArgumentException("Wrong number of parameters in input string");
-        
-        float a;
-        if (!float.TryParse(split[2], out a))
-        {
-            throw new ArgumentException("Split 2 is not a float");
-        }
 
-        float b;
-        if (!float.TryParse(split[4], out b))
+        for (int i = 0; i < (split.Length-1)/2; i++)
         {
-            throw new ArgumentException("Split 4 is not a float");
+            float categoryValue;
+            if (!float.TryParse(split[2*i + 2], out categoryValue))
+            {
+                throw new ArgumentException("Split " + 2*i+2 + " is not a float" );
+            }
+            categories.Add(new Category(split[2 * i + 1], categoryValue));
         }
 
         // Create card:
-        Category cat1 = new Category(split[1], a);
-        Category cat2 = new Category(split[3], b);
-        deck.add(new Card(split[0], cat1, cat2));
+        deck.add(new Card(split[0], categories));
              
 }
 
